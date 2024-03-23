3e12f582f906dc940e2de6bde07ccf3c2b23d042
midCenturyDude7 <mgriffes@gmail.com>
2024-03-23 11:56:38 -0700

    Completed first portion of project 01

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,27 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState("");
+
+  function handleGuess(e) {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  return (
+    <>
+      <form onSubmit={handleGuess}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        />
+      </form>
+    </>
+  );
 }
 
 export default Game;
