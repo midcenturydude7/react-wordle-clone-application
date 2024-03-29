5e1314d1803881bf78e2a5fe7388a4e211fa3b8e
midCenturyDude7 <mgriffes@gmail.com>
2024-03-28 21:34:29 -0700

    Locating bug

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { range } from "../../utils";
 
 function Guess({ value }) {
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import Guess from "../Guess";
-
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
+
+import Guess from "../Guess";
 
 function GuessResults({ guesses }) {
   return (
